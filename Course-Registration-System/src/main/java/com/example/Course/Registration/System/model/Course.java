3262ece368623ddb8ecb99dbd3e98fb8983c1f7c
PRAMILA0509 <pramilavijayamani@gmail.com>
2025-08-07 16:18:32 +0530

    Initial commit

---
+++ Course-Registration-System/src/main/java/com/example/Course/Registration/System/model/Course.java
@@ -0,0 +1,45 @@
+package com.example.Course.Registration.System.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    private  String courseId;
+    private String courseName;
+    private String trainerName;
+    private String courseDuration;
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getTrainerName() {
+        return trainerName;
+    }
+
+    public void setTrainerName(String trainerName) {
+        this.trainerName = trainerName;
+    }
+
+    public String getCourseDuration() {
+        return courseDuration;
+    }
+
+    public void setCourseDuration(String courseDuration) {
+        this.courseDuration = courseDuration;
+    }
+}
